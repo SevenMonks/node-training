73819e3f0a7ee21bc28887bed188fe2159bb82b7
Saptarshi Mandal <saptarshi.mandal@digitalavenues.com>
2019-02-19 19:45:54 +0530

    Started adding validation rules

--- user-management/app/controllers/user/index.controller.js
+++ user-management/app/controllers/user/index.controller.js
@@ -1,5 +1,6 @@
 const express = require('express'),
   router = express.Router(),
+  { body, validationResult } = require('express-validator/check'),
   bcrypt = require('bcrypt-nodejs'),
   config = require('../../../config/config'),
   userModel = require('../../models/user/index.pgsql.model'),
@@ -37,53 +38,86 @@ router.get(
 
 router.post(
   '/users',
+  body('username')
+    .custom((value, { req, loc, path }) => {
+      if (0 === value.replace(/^\s\s*/, '').replace(/\s\s*$/, '').length) {
+        return false;
+      }
+
+      return true;
+    })
+    .withMessage('Username is required')
+    .matches(/^[a-z][a-z0-9]+$/i)
+    .withMessage('Username can only have alphanumeric characters and must start with a letter')
+    .isLength({
+      min: 5
+    })
+    .withMessage('Username cannot be shorter than 5 characters')
+    .isLength({
+      max: 30
+    })
+    .withMessage('Username caannot be longer than 30 characters'),
   async (request, response, next) => {
-    let username = request.sanitize(request.body.username.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
-      password = request.sanitize(request.body.password.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
-      firstName = request.sanitize(request.body.firstName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
-      midName = (request.body.midName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? request.sanitize(request.body.midName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null,
-      lastName = request.sanitize(request.body.lastName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
-      sex = (request.body.sex.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? request.sanitize(request.body.sex.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null,
-      age = (request.body.age.toString().replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? +request.sanitize(request.body.age.toString().replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null;
+    const validationErrors = validationResult(request).mapped();
 
-    password = bcrypt.hashSync(password);
+    if (0 < Object.keys(validationErrors).length) {
+      response.locals.responseStatus = config.app.http_status_codes.http_400_bad_request;
+      response.locals.responseData = {
+        status: 'error',
+        type: config.app.rest_error_codes.invalid_argument,
+        data: {
+          message: validationErrors
+        }
+      };
+      genericErrorHandler(request, response);
+    } else {
+      let username = request.sanitize(request.body.username.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
+        password = request.sanitize(request.body.password.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
+        firstName = request.sanitize(request.body.firstName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
+        midName = (request.body.midName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? request.sanitize(request.body.midName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null,
+        lastName = request.sanitize(request.body.lastName.replace(/^\s\s*/, '').replace(/\s\s*$/, '')),
+        sex = (request.body.sex.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? request.sanitize(request.body.sex.replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null,
+        age = (request.body.age.toString().replace(/^\s\s*/, '').replace(/\s\s*$/, '')) ? +request.sanitize(request.body.age.toString().replace(/^\s\s*/, '').replace(/\s\s*$/, '')) : null;
 
-    try {
-      await userModel.addUser(username, password, firstName, midName, lastName, sex, age)[0];
+      password = bcrypt.hashSync(password);
 
       try {
-        let result = await userModel.getLastInsertId();
+        await userModel.addUser(username, password, firstName, midName, lastName, sex, age)[0];
 
-        response.locals.responseStatus = config.app.http_status_codes.http_200_ok;
-        response.locals.responseData = {
-          status: 'success',
-          type: 'AddNewUser',
-          data: {
-            id: result[0].currval
-          }
-        };
-        successResponseHandler(request, response, next);
+        try {
+          let result = await userModel.getLastInsertId();
+
+          response.locals.responseStatus = config.app.http_status_codes.http_200_ok;
+          response.locals.responseData = {
+            status: 'success',
+            type: 'AddNewUser',
+            data: {
+              id: result[0].currval
+            }
+          };
+          successResponseHandler(request, response, next);
+        } catch (error) {
+          response.locals.responseStatus = config.app.http_status_codes.http_500_internal_server_error;
+          response.locals.responseData = {
+            status: 'error',
+            type: (('development' === process.env.APP_RUNTIME_ENV) ? config.app.database_error_codes.err_level_query_exec : config.app.rest_error_codes.system_error),
+            data: {
+              message: (('development' === process.env.APP_RUNTIME_ENV) ? error.message : 'An internal server error occurred')
+            }
+          };
+          genericErrorHandler(request, response);
+        }
       } catch (error) {
         response.locals.responseStatus = config.app.http_status_codes.http_500_internal_server_error;
         response.locals.responseData = {
           status: 'error',
           type: (('development' === process.env.APP_RUNTIME_ENV) ? config.app.database_error_codes.err_level_query_exec : config.app.rest_error_codes.system_error),
           data: {
-            message: (('development' === process.env.APP_RUNTIME_ENV) ? error.message : 'An internal server error occurred')
+            message: (('development' === process.env.APP_RUNTIME_ENV) ? error.original.message : 'An internal server error occurred')
           }
         };
         genericErrorHandler(request, response);
       }
-    } catch (error) {
-      response.locals.responseStatus = config.app.http_status_codes.http_500_internal_server_error;
-      response.locals.responseData = {
-        status: 'error',
-        type: (('development' === process.env.APP_RUNTIME_ENV) ? config.app.database_error_codes.err_level_query_exec : config.app.rest_error_codes.system_error),
-        data: {
-          message: (('development' === process.env.APP_RUNTIME_ENV) ? error.original.message : 'An internal server error occurred')
-        }
-      };
-      genericErrorHandler(request, response);
     }
   }
 );
--- user-management/app/models/profile/index.pgsql.model.js
+++ user-management/app/models/profile/index.pgsql.model.js
@@ -2,7 +2,7 @@ const db = require('../index');
 
 const updateProfile = (id, username, password, firstName, midName, lastName, sex, age, profileImage) => {
   return db.sequelize.query(
-    "update tbl_user set username = :username, password = :password, first_name = :firstName, mid_name = :midName, last_name = :lastName, sex = :sex, age = :age, profile_image = :profileImage where id = :id",
+    "update tbl_user set username = :username, password = :password, first_name = :firstName, mid_name = :midName, last_name = :lastName, sex = :sex, age = :age, profile_image = :profileImage, updated_on = now() where id = :id",
     {
       replacements: {
         id,
